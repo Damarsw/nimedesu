46eeb6b5076454ccf75bdbc29b19d8c4b7dadb24
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-01 11:29:05 +0700

    Update obfuscate.js

--- obfuscate.js
+++ obfuscate.js
@@ -1,34 +1,32 @@
-const JavaScriptObfuscator = require('javascript-obfuscator');
 const fs = require('fs');
 const path = require('path');
+const { minify } = require('terser');
 
-// Daftar file JavaScript frontend yang akan diproses otomatis saat build
-const filesToProcess = ['script-index.js', 'script-stream.js'];
+async function processFiles() {
+    const filesToProcess = ['script-index.js', 'script-stream.js'];
 
-filesToProcess.forEach(fileName => {
-    const filePath = path.join(__dirname, fileName);
-    
-    if (fs.existsSync(filePath)) {
-        const code = fs.readFileSync(filePath, 'utf8');
-        
-        const obfuscationResult = JavaScriptObfuscator.obfuscate(code, {
-            compact: true, // Minifikasi total (menghapus spasi dan baris baru)
-            controlFlowFlattening: true,
-            controlFlowFlatteningThreshold: 0.75,
-            deadCodeInjection: true,
-            deadCodeInjectionThreshold: 0.4,
-            identifierNamesGenerator: 'dictionary', // Mangling variabel menjadi huruf pendek
-            identifiersDictionary: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'],
-            renameGlobals: false,
-            selfDefending: true,
-            stringArray: true,
-            stringArrayEncoding: ['base64'],
-            stringArrayThreshold: 0.75
-        });
+    for (const file of filesToProcess) {
+        if (fs.existsSync(file)) {
+            console.log(`[INFO] Memproses dan mengamankan ${file}...`);
+            const code = fs.readFileSync(file, 'utf8');
 
-        fs.writeFileSync(filePath, obfuscationResult.getObfuscatedCode(), 'utf8');
-        console.log(`[SUKSES] File ${fileName} berhasil diminifikasi, di-mangle, dan diamankan!`);
-    } else {
-        console.log(`[INFO] File ${fileName} tidak ditemukan.`);
+            const result = await minify(code, {
+                compress: {
+                    drop_console: false, // Ubah jadi true jika ingin menghapus console.log
+                },
+                mangle: {
+                    toplevel: true,
+                },
+            });
+
+            if (result.code) {
+                fs.writeFileSync(file, result.code, 'utf8');
+                console.log(`[SUKSES] ${file} berhasil diamankan.`);
+            }
+        } else {
+            console.log(`[WARNING] File ${file} tidak ditemukan, melewati...`);
+        }
     }
-});
+}
+
+processFiles();
