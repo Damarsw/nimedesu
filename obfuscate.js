3324566ae157d3ffb1144598bed35e7deb483f0f
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-16 12:15:16 +0700

    Update obfuscate.js

--- obfuscate.js
+++ obfuscate.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const JavaScriptObfuscator = require('javascript-obfuscator');
-const { minify } = require('html-minifier-terser');
 
 const jsFiles = ['script-index.js', 'script-stream.js'];
 
@@ -23,31 +22,16 @@ jsFiles.forEach(file => {
 
 const htmlFiles = ['index.html', 'stream.html'];
 
-async function processHtmlFiles() {
-    for (const file of htmlFiles) {
-        if (fs.existsSync(file)) {
-            console.log(`[INFO] Memproses minify & obfuscate untuk ${file}...`);
-            const htmlContent = fs.readFileSync(file, 'utf8');
-            
-            try {
-                const minifiedHtml = await minify(htmlContent, {
-                    collapseWhitespace: true,
-                    removeComments: true,
-                    removeRedundantAttributes: true,
-                    removeScriptTypeAttributes: true,
-                    removeStyleLinkTypeAttributes: true,
-                    useShortDoctype: true,
-                    minifyJS: true,
-                    minifyCSS: true 
-                });
-
-                fs.writeFileSync(file, minifiedHtml, 'utf8');
-                console.log(`[SUKSES] ${file} berhasil di-minify.`);
-            } catch (err) {
-                console.error(`[ERROR] Gagal memproses ${file}:`, err);
-            }
-        }
+htmlFiles.forEach(file => {
+    if (fs.existsSync(file)) {
+        console.log(`[INFO] Memproses enkripsi total untuk ${file}...`);
+        const rawHtml = fs.readFileSync(file, 'utf8');
+        
+        const base64Html = Buffer.from(rawHtml).toString('base64');
+        
+        const encryptedWrapper = `<!DOCTYPE html><html><head><meta charset="UTF-8"><script type="text/javascript">document.write(decodeURIComponent(escape(atob("${base64Html}"))));</script></head><body></body></html>`;
+        
+        fs.writeFileSync(file, encryptedWrapper, 'utf8');
+        console.log(`[SUKSES] ${file} berhasil dienkripsi total.`);
     }
-}
-
-processHtmlFiles();
+});
