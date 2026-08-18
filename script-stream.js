abaf0f0016b224d8272b9bb4cc2494789367d24a
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-19 00:18:18 +0700

    Update script-stream.js

--- script-stream.js
+++ script-stream.js
@@ -1,6 +1,6 @@
 document.addEventListener('contextmenu', function(e) { e.preventDefault(); });
 
-const RENDER_API_URL = "https://nimedesu.onrender.com/api";
+const RENDER_API_URL = "/api-backend";
 let activeEpisodes = [];
 let activeEpisodeIndex = 0;
 let globalAnimeTitle = "Anime";
