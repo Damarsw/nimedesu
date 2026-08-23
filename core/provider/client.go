29276b29bfbbb8c1b4fbf11e1ffdbb3fc7eeb657
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-24 00:20:54 +0700

    Rename SupabaseRequest to DataRequest and update URL

--- core/provider/client.go
+++ core/provider/client.go
@@ -9,15 +9,15 @@ import (
 	"time"
 )
 
-func SupabaseRequest(method, endpoint string, body []byte, headers map[string]string) (*http.Response, error) {
-	reqURL := fmt.Sprintf("%s/rest/v1/%s", strings.TrimRight(env.SupabaseURL, "/"), endpoint)
+func DataRequest(method, endpoint string, body []byte, headers map[string]string) (*http.Response, error) {
+	reqURL := fmt.Sprintf("%s/rest/v1/%s", strings.TrimRight(env.Core, "/"), endpoint)
 	req, err := http.NewRequest(method, reqURL, bytes.NewBuffer(body))
 	if err != nil {
 		return nil, err
 	}
 
-	req.Header.Set("apikey", env.SupabaseKey)
-	req.Header.Set("Authorization", "Bearer "+env.SupabaseKey)
+	req.Header.Set("apikey", env.Token)
+	req.Header.Set("Authorization", "Bearer "+env.Token)
 	req.Header.Set("Content-Type", "application/json")
 
 	for k, v := range headers {
