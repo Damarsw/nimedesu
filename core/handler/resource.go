da503df7d190a4274bee6a929942fe64ff1ff17a
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-24 03:11:51 +0700

    Update resource.go

--- core/handler/resource.go
+++ core/handler/resource.go
@@ -15,12 +15,23 @@ func AnimeListHandler(c *gin.Context) {
 	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
 	perPage, _ := strconv.Atoi(c.DefaultQuery("per_page", "12"))
 	searchQuery := c.Query("q")
+	statusQuery := c.Query("status")
+	genreQuery := c.Query("genre")
 
 	offset := (page - 1) * perPage
-	
+
 	query := fmt.Sprintf("select=id,title,url,status,genre,img_url,score,japanese,total_episodes,duration,release_date,studio&order=id.asc&offset=%d&limit=%d", offset, perPage)
+
 	if searchQuery != "" {
 		query += fmt.Sprintf("&title=ilike.*%s*", url.QueryEscape(searchQuery))
+	}
+
+	if statusQuery != "" {
+		query += fmt.Sprintf("&status=eq.%s", url.QueryEscape(statusQuery))
+	}
+
+	if genreQuery != "" {
+		query += fmt.Sprintf("&genre=ilike.*%s*", url.QueryEscape(genreQuery))
 	}
 
 	resp, err := provider.DataRequest("GET", "anime?"+query, nil, map[string]string{
