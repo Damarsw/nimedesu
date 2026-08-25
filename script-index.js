35a8fe0ad4cadaac5e1098da1ce626794337ebf2
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-26 03:01:10 +0700

    Update script-index.js

--- script-index.js
+++ script-index.js
@@ -525,7 +525,7 @@ function handleAniListOAuthCallback() {
 
 function loginAniList() {
     const resolvedSpecimen = recombineSeedEssence(JACK_SPECIMEN_REF);
-    const authUrl = `https://anilist.co/api/v2/oauth/authorize?client_id=${resolvedSpecimen}&response_type=token`;
+    const authUrl = `https://anilist.co/api/v2/oauth/authorize?client_id=${resolvedSpecimen}&response_type=token&force_login=true`;
     window.location.href = authUrl;
 }
 
