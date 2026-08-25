5094ca0a0ec766eb6842f979a3253f297c49b5fe
dam <127691136+Damarsw@users.noreply.github.com>
2026-08-26 03:32:48 +0700

    Update script-stream.js

--- script-stream.js
+++ script-stream.js
@@ -169,15 +169,11 @@ function loginAniList() {
 }
 
 function logoutAniList() {
-    sessionStorage.removeItem('anilist_token');
-    sessionStorage.removeItem('anilist_user');
-    userBookmarksCache = [];
-    const userWelcomeBanner = document.getElementById('userWelcomeBanner');
-    if (userWelcomeBanner) userWelcomeBanner.classList.add('hidden');
-    const resolvedSpecimen = recombineSeedEssence(JACK_SPECIMEN_REF);
-    const authUrl = `https://anilist.co/api/v2/oauth/authorize?client_id=${resolvedSpecimen}&response_type=token`;
-    
-    window.location.href = authUrl;
+    localStorage.removeItem('anilist_token');
+    localStorage.removeItem('anilist_user');
+
+    alert("Berhasil logout! Silakan login kembali.");
+    window.location.reload();
 }
 
 function getLoggedInUser() {
